4b798c9010411ac60bf94b2891e3ca90a3d12ead
Jeremy Yen <jeremy5189@gmail.com>
2026-09-19 15:27:34 -0400

    Bump version to 3.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = "edgemanage",
-    version = "3.2.0",
+    version = "3.2.1",
     author = "Donncha O Cearbhaill",
     author_email = "donncha@equalit.ie",
     description = "HTTP availability management tool",
